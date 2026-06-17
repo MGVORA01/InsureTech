8dbf933283340675c90ecaf4dfa4a7c2fe1f677e
meet <meetvora.inexture@gmail.com>
2026-06-17 15:33:14 +0530

    feat: update landing page routing and integrate HomePage component

--- insuretech/frontend/src/App.tsx
+++ insuretech/frontend/src/App.tsx
@@ -1,4 +1,5 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import ForgotPasswordPage from './pages/ForgotPasswordPage'
@@ -7,7 +8,7 @@ import ResetPasswordPage from './pages/ResetPasswordPage'
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/forgot-password" element={<ForgotPasswordPage />} />
