e6467b49cbd5bd50a46515f1dddabfd81390deb6
meet <meetvora.inexture@gmail.com>
2026-06-22 19:00:03 +0530

    feat: add admin routes for dashboard and user management

--- insuretech/frontend/src/App.tsx
+++ insuretech/frontend/src/App.tsx
@@ -4,7 +4,12 @@ import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import ForgotPasswordPage from './pages/ForgotPasswordPage'
 import ResetPasswordPage from './pages/ResetPasswordPage'
+import AdminDashboardPage from './pages/AdminDashboardPage'
+import AdminUsersPage from './pages/AdminUsersPage'
 import AuthModal from './features/auth-modal/AuthModal'
+import DashboardPage from './pages/DashboardPage'
+import AdminRoute from './Routes/AdminRoute'
+import ProtectedRoute from './Routes/ProtectedRoute'
 
 function App() {
   const location = useLocation()
@@ -22,6 +27,15 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Route>
+
+        <Route element={<AdminRoute />}>
+          <Route path="/admin/dashboard" element={<AdminDashboardPage />} />
+          <Route path="/admin/users" element={<AdminUsersPage />} />
+        </Route>
       </Routes>
 
       {/* If there was a backgroundLocation (meaning user clicked through), render modal routes on top */}
@@ -46,58 +60,6 @@ function App() {
         </Routes>
       ) : null}
 
-      {/* Show modal only when opened from another page */}
-      {/*{background && (*/}
-      {/*  <Routes>*/}
-      {/*    <Route*/}
-      {/*      path="/login"*/}
-      {/*      element={*/}
-      {/*        <AuthModal*/}
-      {/*          initialTab="login"*/}
-      {/*          onClose={() =>*/}
-      {/*            navigate(background.pathname + background.search)*/}
-      {/*          }*/}
-      {/*        />*/}
-      {/*      }*/}
-      {/*    />*/}
-
-      {/*    <Route*/}
-      {/*      path="/register"*/}
-      {/*      element={*/}
-      {/*        <AuthModal*/}
-      {/*          initialTab="register"*/}
-      {/*          onClose={() =>*/}
-      {/*            navigate(background.pathname + background.search)*/}
-      {/*          }*/}
-      {/*        />*/}
-      {/*      }*/}
-      {/*    />*/}
-
-      {/*    <Route*/}
-      {/*      path="/forgot-password"*/}
-      {/*      element={*/}
-      {/*        <AuthModal*/}
-      {/*          initialTab="forgotPassword"*/}
-      {/*          onClose={() =>*/}
-      {/*            navigate(background.pathname + background.search)*/}
-      {/*          }*/}
-      {/*        />*/}
-      {/*      }*/}
-      {/*    />*/}
-
-      {/*    <Route*/}
-      {/*      path="/reset-password"*/}
-      {/*      element={*/}
-      {/*        <AuthModal*/}
-      {/*          initialTab="resetPassword"*/}
-      {/*          onClose={() =>*/}
-      {/*            navigate(background.pathname + background.search)*/}
-      {/*          }*/}
-      {/*        />*/}
-      {/*      }*/}
-      {/*    />*/}
-      {/*  </Routes>*/}
-      {/*)}*/}
     </>
   )
 }
